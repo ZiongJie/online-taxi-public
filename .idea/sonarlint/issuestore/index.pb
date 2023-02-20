95acec886af158cf0b9fb3f8fd5dbb2dd9cd8723
zhangjie <jie.zhang51@caocaoglobal.com>
2023-02-20 21:42:00 +0800

    服务调用准备

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+E
+api-passenger/pom.xml,4/1/41b04181b17563f2ab260763db88345d2b7305af
+Š
+Zservice-verificaioncode/src/main/java/com/mashibing/service/controller/TestController.java,0/3/03e491a25233a26282bc17624e7dd117320b5e53
+ƒ
+Sapi-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java,e/c/ec8c720c79264898b0c3f70a579ad0fa3b3eff17
+ˆ
+Xservice-verificaioncode/src/main/java/com/mashibing/service/ServiceVerificationcode.java,9/d/9d1d43364acefdbb9f3ae8bd11484e982d49a28d
---
+++ service-verificaioncode/src/main/java/com/mashibing/service/controller/NumberCodeController.java
@@ -0,0 +1,33 @@
+package com.mashibing.service.controller;
+
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.response.NumberCodeResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NumberCodeController {
+
+    @GetMapping("/numberCode/{size}")
+    public ResponseResult number(@PathVariable("size") int size) {
+
+        System.out.println("size:"+size);
+        // 生成验证码
+        double mathRandom = (Math.random() * 9 + 1) * (Math.pow(10,size - 1));
+        System.out.println(mathRandom);
+        int resultInt = (int)mathRandom;
+        System.out.println(resultInt);
+
+        // 定义返回值
+        NumberCodeResponse response = new NumberCodeResponse();
+        response.setNumberCode(resultInt);
+
+        return ResponseResult.success(response);
+    }
+
+    public static void main(String[] args) {
+        // 获取随机数
+
+    }
+}
