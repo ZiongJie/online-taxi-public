fd1d33e53a960e1753cefa82586942c228521c49
zhangjie <jie.zhang51@caocaoglobal.com>
2023-02-25 18:38:01 +0800

    乘客端验证发送完成

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,8 +1,6 @@
 
 Š
 Zservice-verificaioncode/src/main/java/com/mashibing/service/controller/TestController.java,0/3/03e491a25233a26282bc17624e7dd117320b5e53
-‚
-Rinternal-common/src/main/java/com/mashibing/internalcommon/dto/ResponseResult.java,4/2/4242e64678f593130bbd3a5bd8ba8e7ebb816f79
 ‰
 Yinternal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java,8/3/83c160a70c9f286c8806fcde6dffb5098c977c16
 ‹
--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -5,7 +5,10 @@ import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.response.NumberCodeResponse;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class VerificationCodeService {
@@ -13,12 +16,17 @@ public class VerificationCodeService {
     @Autowired
     private ServiceVerificationcodeClient serviceVerificationcodeClient;
 
-    public String generationCode(String passengerPhone) {
+    private String verificationcodePrefix = "passenger-verification-code-";
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    public ResponseResult generationCode(String passengerPhone) {
 
         // 调用验证码服务，获取验证码
         System.out.println("调用验证码服务，获取验证码");
 
-        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationcodeClient.getNumberCode();
+        ResponseResult<NumberCodeResponse> numberCodeResponse = serviceVerificationcodeClient.getNumberCode(6);
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
         System.out.println("remote nmber code:" + numberCode);
@@ -27,11 +35,15 @@ public class VerificationCodeService {
 
         // 存入redis
         System.out.println("存入redis");
+        // key,value,过期时间
+        String key = verificationcodePrefix + passengerPhone;
+        // 存入redis
+        stringRedisTemplate.opsForValue().set(key, numberCode+"", 2, TimeUnit.MINUTES);
+
+        // 通过短信服务商，将对应的验证码发送到手机上。阿里短信服务，腾讯短信通，华信，容联
+
 
         // 返回值
-        JSONObject result = new JSONObject();
-        result.put("code", 1);
-        result.put("message", "success");
-        return result.toString();
+        return ResponseResult.success();
     }
 }
