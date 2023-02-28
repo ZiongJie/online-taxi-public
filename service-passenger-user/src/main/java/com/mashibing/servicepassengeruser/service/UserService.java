a6f780799aa74312023c3af83dcdb6da2719b10a
zhangjie <jie.zhang51@caocaoglobal.com>
2023-02-28 21:43:22 +0800

    根据手机号查询用户信息

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -1,17 +1,32 @@
 package com.mashibing.servicepassengeruser.service;
 
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.servicepassengeruser.dto.PassengerUser;
+import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
 
-    public ResponseResult loginOrReg(String passengerPhone) {
-        System.out.println("user service被调用, 手机号" + passengerPhone);
-        // 根据手机号查询用户信息
+    @Autowired
+    private PassengerUserMapper passengerUserMapper;
 
+    public ResponseResult loginOrReg(String passengerPhone) {
+
+        System.out.println("user service被调用, 手机号" + passengerPhone);
+
+        // 根据手机号查询用户信息
+        Map<String, Object> map = new HashMap<>();
+        map.put("passenger_phone", passengerPhone);
+        List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
+        System.out.println(passengerUsers==null?"无记录":passengerUsers.get(0).getPassengerPhone());
         // 判读用户信息是否存在
-        
+
         // 如果不存在，输入用户信息
 
         return ResponseResult.success();
