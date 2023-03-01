58e85d95846e74860f6cbda2484cf7965d53efe6
zhangjie <jie.zhang51@caocaoglobal.com>
2023-03-01 20:51:22 +0800

    乘客api调用乘客用户服务

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -1,8 +1,10 @@
 package com.mashibing.apipassenger.service;
 
+import com.mashibing.apipassenger.remote.ServicePassengerUserClient;
 import com.mashibing.apipassenger.remote.ServiceVerificationcodeClient;
 import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.request.VerificationCodeDTO;
 import com.mashibing.internalcommon.response.NumberCodeResponse;
 import com.mashibing.internalcommon.response.TokenResponse;
 import org.apache.commons.lang.StringUtils;
@@ -53,6 +55,18 @@ public class VerificationCodeService {
     }
 
     /**
+     * 根据手机号，生成key
+     * @param passengerPhone
+     * @return
+     */
+    private String generatorKeyByPhone(String passengerPhone) {
+        return verificationcodePrefix + passengerPhone;
+    }
+
+    @Autowired
+    private ServicePassengerUserClient servicePassengerUserClient;
+
+    /**
      * 校验验证码
      * @param passengerPhone 手机号
      * @param verificationCode 验证码
@@ -73,11 +87,14 @@ public class VerificationCodeService {
             return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
         }
         // trim删除字符串前后的空白符
-        if (verificationCode.trim().equals(codeRedis.trim())) {
+        if (!verificationCode.trim().equals(codeRedis.trim())) {
             return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(), CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
         }
 
         // 判断原来是否有用户，并进行对应的处理
+        VerificationCodeDTO verificationCodeDTO = new VerificationCodeDTO();
+        verificationCodeDTO.setPassengerPhone(passengerPhone);
+        servicePassengerUserClient.loginOrRegister(verificationCodeDTO);
 
         // 颁发令牌
         System.out.println("颁发令牌");
@@ -86,14 +103,5 @@ public class VerificationCodeService {
         TokenResponse tokenResponse = new TokenResponse();
         tokenResponse.setToken("token value");
         return ResponseResult.success(tokenResponse);
-    }
-
-    /**
-     * 根据手机号，生成key
-     * @param passengerPhone
-     * @return
-     */
-    private String generatorKeyByPhone(String passengerPhone) {
-        return verificationcodePrefix + passengerPhone;
     }
 }
