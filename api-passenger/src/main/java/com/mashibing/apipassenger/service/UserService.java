782be040e1fcb9aab206e5f076136615141e583c
zhangjie <jie.zhang51@caocaoglobal.com>
2023-03-19 20:19:46 +0800

    service-passenger-user根据手机号查询用户信息

--- api-passenger/src/main/java/com/mashibing/apipassenger/service/UserService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/UserService.java
@@ -2,6 +2,8 @@ package com.mashibing.apipassenger.service;
 
 import com.mashibing.internalcommon.dto.PassengerUser;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.dto.TokenResult;
+import com.mashibing.internalcommon.util.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +14,13 @@ public class UserService {
     public ResponseResult getUserByAccessToken(String accessToken) {
         log.info("accessToken:" + accessToken);
         // 解析accessToken 拿到手机号
+        TokenResult tokenResult = JwtUtils.checkToken(accessToken);
+        String phone = tokenResult.getPhone();
+        log.info("手机号:" + phone);
 
         // 根据手机号查询用户信息
+
+
         PassengerUser passengerUser = new PassengerUser();
         passengerUser.setPassengerName("张三");
         passengerUser.setProfilePhoto("头像");
