22172e68737340db0a98801f07b1fcc17826a560
zhangjie <jie.zhang51@caocaoglobal.com>
2023-03-21 21:52:47 +0800

    service-map 项目创建

--- service-price/src/main/java/com/mashibing/serviceprice/controller/ForecastPriceController.java
+++ service-price/src/main/java/com/mashibing/serviceprice/controller/ForecastPriceController.java
@@ -1,26 +1,21 @@
-package com.mashibing.apipassenger.controller;
+package com.mashibing.serviceprice.controller;
 
-import com.mashibing.apipassenger.service.ForecastPriceService;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.ForecastPriceDTO;
-import lombok.extern.slf4j.Slf4j;
+import com.mashibing.serviceprice.service.ForecastPriceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
 @RestController
-@Slf4j
 public class ForecastPriceController {
 
     @Autowired
     private ForecastPriceService forecastPriceService;
 
-    @PostMapping("/forecast-price")
-    public ResponseResult forecastPrice(@RequestBody ForecastPriceDTO forecastPriceDTO) {
-
+    @PostMapping("forecast-price")
+    public ResponseResult forecastPrice(@RequestBody ForecastPriceDTO forecastPriceDTO){
         String depLongitude = forecastPriceDTO.getDepLongitude();
         String depLatitude = forecastPriceDTO.getDepLatitude();
         String destLongitude = forecastPriceDTO.getDestLongitude();
