77eebe68c1a4f465e3ccc5a16a817d2c29c41073
zhangjie <jie.zhang51@caocaoglobal.com>
2023-04-02 11:27:22 +0800

    调用地图接口获取路径规划信息

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/constant/AmapConfigConstants.java
@@ -0,0 +1,24 @@
+package com.mashibing.internalcommon.constant;
+
+public class AmapConfigConstants {
+    /**
+     * 路径规划地址
+     */
+    public static final String DIRECTION_URL = "http://restapi.amap.com/v3/direction/driving";
+
+    /**
+     * 路径规划 json key值
+     */
+    public static final String STATUS = "status";
+
+    /**
+     * 路径规划
+     */
+    public static final String ROUTE = "route";
+
+    public static final String PATHS = "paths";
+
+    public static final String DISTANCE = "distance";
+
+    public static final String DURATION = "duration";
+}
